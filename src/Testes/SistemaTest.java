44d16515a06bddc8a1d746777429dd171d348ad3
Matheus7OP <matheus7op@gmail.com>
2017-12-19 17:51:06 -0300

    Adicionado testes da classe Sistema

--- src/Testes/SistemaTest.java
+++ src/Testes/SistemaTest.java
@@ -1,5 +1,43 @@
 package Testes;
 
-public class SistemaTest {
+import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import lab5.Sistema;
+
+public class SistemaTest {
+	private Sistema sistema;
+	
+	@Before
+	public void inicializar() {
+		this.sistema = new Sistema(10000, 0.1);
+	}
+	
+	@Test
+	public void getCaixa() {
+		assertEquals(this.sistema.getCaixa(), 10000);
+	}
+	
+	@Test
+	public void getCaixaCenario() {
+		this.sistema.cadastrarCenario("Irá chover hoje.");
+		this.sistema.cadastrarAposta(1, "Matheus", 1500, "VAI ACONTECER");
+		this.sistema.fecharAposta(1, false);
+		
+		assertEquals(this.sistema.getCaixaCenario(1), 150);
+	}
+	
+	@Test
+	public void getTotalRateioTest() {
+		this.sistema.cadastrarCenario("Irá chover hoje.");
+		
+		this.sistema.cadastrarAposta(1, "Matheus", 1500, "VAI ACONTECER");
+		this.sistema.cadastrarAposta(1, "Victor", 250000, "N VAI ACONTECER");
+		
+		this.sistema.fecharAposta(1, true);
+		
+		assertEquals(this.sistema.getTotalRateioCenario(1), (250000 - 25000));
+	}
 }
